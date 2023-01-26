b5877bf7347749d2420422da63c806f784ee83bd
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2023-01-26 11:03:51 -0500

    commit tree 1a57d659bb7ec19215756c6eb6b4c65cd7521aa1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # User Routes
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  
+  # Session Routes
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  # Recipe Routes 
+  resources :recipes, only: [:index, :create]
+  # get "/recipes", to: "recipes#index"
+  # post "/recipes", to: "recipes#create"
 end
